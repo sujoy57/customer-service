853be2ba05f99eb57da43a788b889a24523ef2a1
sujoy <sujoy@SUJOY-PC.Home>
2024-12-01 15:54:34 +0000

    added terraform file

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_cloud_run_service" "my_service" {
+  name     = var.service_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = var.region
+  service     = google_cloud_run_service.my_service.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+variable "project_id" {}
+variable "region" {}
+variable "service_name" {}
+variable "image" {}
