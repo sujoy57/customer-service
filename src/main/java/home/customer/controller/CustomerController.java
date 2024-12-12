861d46781eb0601da010ad999753ead85c93358c
sujoy <sujoy@SUJOY-PC.Home>
2024-12-12 11:48:47 +0000

    changed message

--- src/main/java/home/customer/controller/CustomerController.java
+++ src/main/java/home/customer/controller/CustomerController.java
@@ -23,7 +23,7 @@ public class CustomerController {
     
     @GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome Sujoy abc";
+		return "Welcome Sujoy abcd";
 	}
 
     /*@GetMapping("/{id}")
